d0f04d299baef0dac67bfd6a026f09c5fe209f18
vagrant <mikefyke@hotmail.com>
2019-04-17 22:10:32 +0000

    Finished Day 2 adding tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,74 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Fake data taken from tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
+
+$(document).ready(function() {
+  console.log('DOM Content Loaded');
+
+  function renderTweets(data) {
+    data.forEach( (elements) => {
+      $('#tweets-container').append(createTweetElement(elements));
+    })
+  }
+
+  function createTweetElement(tweetData) {
+    const tweet = tweetData.content.text;
+    const name = tweetData.user.name;
+    const avatarsImg = tweetData.user.avatars.small;
+    const tweeHandle = tweetData.user.handle;
+
+    const $createAvatar = $('<img>').addClass("avatar").attr("src",avatarsImg);
+    const $createName = $('<div>').addClass("tweeterName").text(name);
+    const $createTweet = $('<div>').addClass("tweeBody").text(tweet);
+    const $createHandle = $('<p>').addClass("userTwee").text(tweeHandle);
+
+    const tweets = $('<article class="tweets">').append($createAvatar).append($createName).append($createHandle).append($createTweet);
+    return tweets
+  }
+  renderTweets(data);
+});
