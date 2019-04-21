c1433b2dc05408becce41b409357d69fdfd9055b
vagrant <mikefyke@hotmail.com>
2019-04-21 00:49:17 +0000

    Stylized my page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 
     $("#composeTweet").click(function() {
     $(".new-tweet").slideToggle("slow")
+    $(".textArea").focus();
     });
 
   function renderTweets(data) {
@@ -12,6 +13,31 @@ $(document).ready(function() {
       $('#tweets-container').prepend(createTweetElement(tweet));
     })
   }
+
+  function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = Math.floor(seconds / 31536000);
+  if (interval >= 1) {
+      return interval + " years ago";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval >= 1) {
+      return interval + " months ago";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval >= 1) {
+      return interval + " days ago";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval >= 1) {
+      return interval + " hours ago";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval >= 1) {
+      return interval + " minutes ago";
+  }
+  return Math.floor(seconds + 1) + " seconds ago";
+}
 
   function createTweetElement(tweetData) {
     const tweet = tweetData.content.text;
@@ -33,12 +59,14 @@ $(document).ready(function() {
     const $header = $("<header>");
     const $footer = $("<footer>");
     const $content = $("<div>").addClass("content");
+    const $dateOfTweet = $("<h6>").text(timeSince(tweetData['created_at']));
 
     $iconContainer.append($flag, $retweet, $heart);
     $header.append($createAvatar, $createName, $createHandle);
     $content.append($createTweet);
-    $footer.append($iconContainer);
+    $footer.append($iconContainer, $dateOfTweet);
     $tweetData.append($header, $content, $footer);
+
 
     return $tweetData;
   }
@@ -51,12 +79,12 @@ $(document).ready(function() {
 
       if(!$('textarea', this).val()){
        $(".error").slideDown("slow");
-       $('#specific-message').text('Empty input');
+       $('#specific-message').text('Error : Empty input');
       };
 
       if($('textarea', this).val().length > 140){
         $(".error").slideDown("slow");
-      $('#specific-message').text('Too many characters')
+      $('#specific-message').text('Error : Too many characters')
       };
 
       if(($('textarea', this).val()) && ($('textarea', this).val().length < 140)){
